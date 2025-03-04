503954e94028c02b5b49439f77e40121bc834182
yeji-star <sizuku134@gmail.com>
2025-03-04 15:59:41 +0900

    if문

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,53 @@
 package org.example;
 
-// 문제 : 실행되는 출력문에는 참 그렇지 않으면 거짓 이라고 적어주세요.
+// 문제 : 할인 대상인지 아닌지 출력해주세요.
+// 조건 : 나이가 19세 이하이거나 60세 이상이면 할인 대상입니다.
+// 조건 : 출력예시 처럼 출력되어야 합니다.
+// 조건 : `구현시작` 부분만 수정 할 수 있습니다.
+// 조건 : 4가지 이상의 방법으로 풀어야 합니다.
+// 조건 : 그 중 2가지 방법은 `&&, ||`없이 풀어야 합니다.
 
 class Main {
     public static void main(String[] args) {
+        int age = 70;// 이 값을 바꿔가면서 실행해보세요.
 
+        System.out.println("당신의 나이는 " + age + "살 입니다.");
 
+        // 구현시작
 
+        System.out.println("v1");
+
+        if (19 >= age) {
+            System.out.println("할인대상");
+        } else if (60 <= age) {
+            System.out.println("할인 대상입니다.");
+        } else {
+            System.out.println("할인 대상이 아닙니다.");}
+
+        System.out.println("v2");
+
+        if (20 > age || 59 < age) {
+            System.out.println("할인대상");
+        } else {
+            System.out.println("할인 대상이 아닙니다.");}
+
+        System.out.println("v3");
+
+        if (20 <= age && 60 > age) {
+            System.out.println("할인 대상이 아닙니다.");
+        } else {
+            System.out.println("할인대상");
+        }
+
+        System.out.println("v4");
+
+        if (60 > age) {
+            System.out.println("할인대상 아님");
+        } if (20 <= age) {
+            System.out.println("할인대상 아님");
+        } else {
+            System.out.println("할인 대상");}
+
+        // 출력 => 할인대상입니다. 또는 할인 대상이 아닙니다.
     }
 }
