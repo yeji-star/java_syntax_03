f5d7cd72e4a5b2bf25f64de5c1c705fc44b13715
yeji-star <sizuku134@gmail.com>
2025-03-04 14:23:15 +0900

    조건식_논리 연산자 문제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,75 +4,8 @@ package org.example;
 
 class Main {
     public static void main(String[] args) {
-        if ( true ) {
-            System.out.println("참");
-        }
 
-        if ( false ) {
-            System.out.println("거짓");
-        }
 
-        int a = 10;
 
-        // `==` => 같다.
-        if ( a == 10 ) {
-            System.out.println("참1");
-        }
-
-        // `!=` => 같지 않다.
-        if ( a != 10 ) {
-            System.out.println("거짓2");
-        }
-
-        if ( a > 10 ) {
-            System.out.println("거짓3");
-        }
-
-        if ( a >= 10 ) {
-            System.out.println("참4");
-        }
-
-        int b = 10;
-
-        if ( a == b ) {
-            System.out.println("참5");
-        }
-
-        // boolean c => c 에는 오직 true/false 만 담을 수 있다.
-        // 오직 참/거짓(논리)만 넣을 수 있는 c라는 이름의 변수에 a != b를 넣겠다.
-
-        boolean c = a != b;
-
-        if ( c ) {
-            System.out.println("거짓6");
-        }
-
-        if ( c == false ) {
-            System.out.println("참7");
-        }
-
-        // `!` => 반전
-        if ( !c ) {
-            System.out.println("참8");
-        }
-
-        // `!` => 반전
-        if ( !(!c) ) {
-            System.out.println("거짓9");
-        }
-
-        boolean d = true;
-
-        if ( c != d ) {
-            System.out.println("참10");
-        }
-
-        if ( 20 > 2 && 10 > 3 && true != false && 10 != 10 ) {
-            System.out.println("거짓11");
-        }
-
-        if ( 10 != 10 || 10 < 2 ) {
-            System.out.println("거짓12");
-        }
     }
 }
