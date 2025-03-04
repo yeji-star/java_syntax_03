dcc9633804968cd6a11050a77e6a0491bf43febb
yeji-star <sizuku134@gmail.com>
2025-03-04 10:50:32 +0900

    num

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,35 @@
 package org.example;
 
-// 문제 : `안녕하세요.`를 10번 출력해주세요.
+// 문제 : a와 b가 가지고 있는 값을 서로 뒤바꿔주세요.
 
 class Main {
     public static void main(String[] args) {
+        // `int a` => a라는 이름의 변수(즉 공간)을 만든다.
+        // `int` => a라는 공간에는 정수만 담을 수 있다.
+        // `;` => 문장이 끝남을 알린다.
+        int a;
+        a = 5;
 
-        System.out.print("안녕하세요.\n안녕하세요." +
-                "\n안녕하세요.\n안녕하세요.\n 안녕하세요.\n안녕하세요.\n안녕하세요.\n");
+        System.out.println(a);
+        System.out.println(a + 10);
+
+        int b = 10;
+
+        // `+` => 문장과 문자을 합친다.
+        System.out.println("a : " + a);
+        System.out.println("b : " + b);
+
+        // 여기서 부터
+
+        a = a + 5;
+        b = b - 5;
+
+
+        // 여기까지 수정 가능
+
+        System.out.println("a : " + a);
+        // 출력 : a : 10
+        System.out.println("b : " + b);
+        // 출력 :  b : 5
     }
 }
