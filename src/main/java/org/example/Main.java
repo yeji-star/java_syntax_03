06de9631c32462fa41e056c7491318b8722dc90e
yeji-star <sizuku134@gmail.com>
2025-03-04 14:13:34 +0900

    조건식_논리 연산자 문제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,78 @@
 package org.example;
 
-// 조건문의 가장 기본적인 형태
+// 문제 : 실행되는 출력문에는 참 그렇지 않으면 거짓 이라고 적어주세요.
 
 class Main {
     public static void main(String[] args) {
-
-        if (10 > 20 || 1 != 1 && 3>2) {
-
-            System.out.println("실행됨");
-
-        } else {
-            System.out.println("실행불가");
+        if ( true ) {
+            System.out.println("참");
         }
 
+        if ( false ) {
+            System.out.println("거짓");
+        }
+
+        int a = 10;
+
+        // `==` => 같다.
+        if ( a == 10 ) {
+            System.out.println("참1");
+        }
+
+        // `!=` => 같지 않다.
+        if ( a != 10 ) {
+            System.out.println("거짓2");
+        }
+
+        if ( a > 10 ) {
+            System.out.println("거짓3");
+        }
+
+        if ( a >= 10 ) {
+            System.out.println("참4");
+        }
+
+        int b = 10;
+
+        if ( a == b ) {
+            System.out.println("참5");
+        }
+
+        // boolean c => c 에는 오직 true/false 만 담을 수 있다.
+        // 오직 참/거짓(논리)만 넣을 수 있는 c라는 이름의 변수에 a != b를 넣겠다.
+
+        boolean c = a != b;
+
+        if ( c ) {
+            System.out.println("거짓6");
+        }
+
+        if ( c == false ) {
+            System.out.println("참7");
+        }
+
+        // `!` => 반전
+        if ( !c ) {
+            System.out.println("참8");
+        }
+
+        // `!` => 반전
+        if ( !(!c) ) {
+            System.out.println("거짓9");
+        }
+
+        boolean d = true;
+
+        if ( c != d ) {
+            System.out.println("참10");
+        }
+
+        if ( 20 > 2 && 10 > 3 && true != false && 10 != 10 ) {
+            System.out.println("거짓11");
+        }
+
+        if ( 10 != 10 || 10 < 2 ) {
+            System.out.println("거짓12");
+        }
     }
 }
