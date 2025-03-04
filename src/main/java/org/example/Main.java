245388e69299528b712fc1a7ab947064de4c2eee
yeji-star <sizuku134@gmail.com>
2025-03-04 09:58:53 +0900

    03

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+// 문제 : `안녕하세요.`를 10번 출력해주세요.
+
+class Main {
+    public static void main(String[] args) {
+
+        System.out.print("안녕하세요.\n안녕하세요.\n안녕하세요.\n안녕하세요. " +
+                "안녕하세요.\n안녕하세요.\n안녕하세요.\n");
+    }
+}
