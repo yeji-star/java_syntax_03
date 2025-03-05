1838f84262f07644e433f7bb47c50de673d05979
yeji-star <sizuku134@gmail.com>
2025-03-05 09:10:51 +0900

    구구단

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,53 +1,31 @@
 package org.example;
 
-// 문제 : 할인 대상인지 아닌지 출력해주세요.
-// 조건 : 나이가 19세 이하이거나 60세 이상이면 할인 대상입니다.
-// 조건 : 출력예시 처럼 출력되어야 합니다.
-// 조건 : `구현시작` 부분만 수정 할 수 있습니다.
-// 조건 : 4가지 이상의 방법으로 풀어야 합니다.
-// 조건 : 그 중 2가지 방법은 `&&, ||`없이 풀어야 합니다.
+
+// 문제 : 아래와 같이 출력해주세요.
 
 class Main {
     public static void main(String[] args) {
-        int age = 70;// 이 값을 바꿔가면서 실행해보세요.
+        System.out.println("8 * 1 = 8\n" +
+                "    8 * 2 = 16\n" +
+                "    8 * 3 = 24\n" +
+                "    8 * 4 = 32\n" +
+                "    8 * 5 = 40\n" +
+                "    8 * 6 = 48\n" +
+                "    8 * 7 = 56\n" +
+                "    8 * 8 = 64\n" +
+                "    8 * 9 = 72");
 
-        System.out.println("당신의 나이는 " + age + "살 입니다.");
-
-        // 구현시작
-
-        System.out.println("v1");
-
-        if (19 >= age) {
-            System.out.println("할인대상");
-        } else if (60 <= age) {
-            System.out.println("할인 대상입니다.");
-        } else {
-            System.out.println("할인 대상이 아닙니다.");}
-
-        System.out.println("v2");
-
-        if (20 > age || 59 < age) {
-            System.out.println("할인대상");
-        } else {
-            System.out.println("할인 대상이 아닙니다.");}
-
-        System.out.println("v3");
-
-        if (20 <= age && 60 > age) {
-            System.out.println("할인 대상이 아닙니다.");
-        } else {
-            System.out.println("할인대상");
-        }
-
-        System.out.println("v4");
-
-        if (60 > age) {
-            System.out.println("할인대상 아님");
-         if (20 <= age) {
-            System.out.println("할인대상 아님");
-        }} else {
-            System.out.println("할인 대상");}
-
-        // 출력 => 할인대상입니다. 또는 할인 대상이 아닙니다.
+        // 출력
+    /*
+    8 * 1 = 8
+    8 * 2 = 16
+    8 * 3 = 24
+    8 * 4 = 32
+    8 * 5 = 40
+    8 * 6 = 48
+    8 * 7 = 56
+    8 * 8 = 64
+    8 * 9 = 72
+    */
     }
 }
---
+++ src/main/java/org/example/Main4.java
@@ -0,0 +1,37 @@
+package org.example;
+
+// 문제 : 아래와 같이 출력해주세요.
+// 문제 : 2부터 9까지의 숫자를 사용하지 않고 구구단 출력을 완성 해주세요.
+
+class Main4 {
+    public static void main(String[] args) {
+        int dan = 8;
+
+        // 수정가능지역 시작
+        int i = 1;
+
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        System.out.println(dan + " * " + ( i + i )+ " = " + dan * ( i + i ));
+        System.out.println(dan + " * " + ( i + i + i )+ " = " + dan * ( i + i + i ));
+        System.out.println(dan + " * " + ( i + i + i + i )+ " = " + dan * ( i + i + i + i ));
+        System.out.println(dan + " * " + ( i + i + i + i + i )+ " = " + dan * ( i + i + i + i + i ));
+        System.out.println(dan + " * " + ( i + i + i + i + i + i )+ " = " + dan * ( i + i + i + i + i + i ));
+        System.out.println(dan + " * " + ( i + i + i + i + i + i + i )+ " = " + dan * ( i + i + i + i + i + i + i ));
+        System.out.println(dan + " * " + ( i + i + i + i + i + i + i + i )+ " = " + dan * ( i + i + i + i + i + i + i + i ));
+        System.out.println(dan + " * " + ( i + i + i + i + i + i + i + i + i )+ " = " + dan * ( i + i + i + i + i + i + i + i + i ));
+        // 수정가능지역 끝
+
+        // 출력
+    /*
+    8 * 1 = 8
+    8 * 2 = 16
+    8 * 3 = 24
+    8 * 4 = 32
+    8 * 5 = 40
+    8 * 6 = 48
+    8 * 7 = 56
+    8 * 8 = 64
+    8 * 9 = 72
+    */
+    }
+}
