bc03333a16d6d039ea24c6540d25bbe3ddd8d76f
yeji-star <sizuku134@gmail.com>
2025-03-05 16:19:14 +0900

    예제

--- src/main/java/org/example/Main5.java
+++ src/main/java/org/example/Main5.java
@@ -4,15 +4,18 @@ package org.example;
 class Main5 {
     public static void main(String[] args) {
 
-        int age = 25;
-        double income = 55000;
-        boolean hasCriminalRecord = false;
+        char c = 'a';
 
-        if(age > 21 &&  (income > 50000 || income <= 0) && !hasCriminalRecord) {
-            System.out.println("dd");
-        } else {
-            System.out.println("ss");
-        }
+        System.out.println(c);
+        System.out.printf("%c\n", c);
+        // %는 지정자. %c는 문자 지정자. 이 문장은 c라는 서식으로 "%c\n"을 출력하겠다.
+
+        int a = 97;
+
+        System.out.println(a);
+        System.out.printf("%d", a);
+        //decimal <- 10진수(정수) 그렇기에 %d로 해서 정수 지정자를 한 것.
+        //%c면 그냥 a만 출력된다.
 
 
     }
