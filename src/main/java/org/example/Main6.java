4c61fba2bceb2f4696d2d63f1d36bf24ca0fb023
yeji-star <sizuku134@gmail.com>
2025-03-05 11:45:37 +0900

    구구단 + while문 입문

--- src/main/java/org/example/Main6.java
+++ src/main/java/org/example/Main6.java
@@ -3,13 +3,20 @@ package org.example;
 class Main6 {
     public static void main(String[] args) {
 
-        int i = 10; // 초기 (값)
+        int i = 5; // 초기 (값)
 
-        while (i >= 5) { //(종료) 조건
-            System.out.println(10);
-            i--; //step(보폭, 방향)
+        while (i == 4) { //(종료) 조건
+            System.out.println(i);
+            i+= 4; //step(보폭, 방향)
         }
 
-        System.out.println("i = " + i);
+        int j = 10;
+
+        while (j<=3) {
+            System.out.println(2);
+            i++; // **중요**
+        }
+
+        System.out.println("i = " + j);
     }
     }
